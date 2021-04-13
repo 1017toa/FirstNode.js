0f67f5fff4405c7fe10276c9fac6a86b7f5f9498
1017toa <1017toa@naver.com>
2021-04-13 22:22:14 +0900

    section2 DONE

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -2,6 +2,17 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+	host : 'localhost',
+	port : 3306,
+	user : 'root',
+	password : '123',
+	database : 'jsman'
+})
+
+connection.connect()
 
 app.listen(3000, function() {
 	console.log("start! express server on port 3000");
@@ -13,8 +24,12 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
 
 app.get('/', function(req,res) {
-	res.sendFile(__dirname + "/publc/main.html")
-})
+	res.sendFile(__dirname + "/public/main.html")
+});
+
+app.get('/main', function(req,res) {
+	res.sendFile(__dirname + "/public/main.html")
+});
 
 app.post('/email_post', function(req,res){
 	//get : req.param('email')
@@ -24,7 +39,18 @@ app.post('/email_post', function(req,res){
 })
 
 app.post('/ajax_send_email', function(req, res){
-	console.log(req.body.email);
-	var responseData = {'result' : 'ok', 'email' : req.body.email};
-	res.json(responseData);
+	var email = req.body.email;
+	var responseData = {};
+
+	var query = connection.query('select name from user where email="'+ email +'"', function(err, raws){
+		if(err) throw err;
+		if(raws[0]) {
+			responseData.result = "ok";
+			responseData.name = raws[0].name;
+		} else {
+			responseData.result = "name";
+			responseData.name = "";
+		}
+		res.json(responseData)
+	})
 });
